e6b93c145b81f684f9d1db7a20725cdde33e989a
soham70 <87270632+soham70@users.noreply.github.com>
2026-08-26 15:47:13 +0530

    Enhance navigation and add scroll effects
    Added mobile navigation toggle, scroll effects, and reveal animations.

--- index.js
+++ index.js
@@ -1,56 +1,112 @@
-var TxtType = function(el, toRotate, period) {
-    this.toRotate = toRotate;
-    this.el = el;
-    this.loopNum = 0;
-    this.period = parseInt(period, 10) || 2000;
-    this.txt = '';
-    this.tick();
-    this.isDeleting = false;
+/* Typewriter effect */
+var TxtType = function (el, toRotate, period) {
+  this.toRotate = toRotate;
+  this.el = el;
+  this.loopNum = 0;
+  this.period = parseInt(period, 10) || 2000;
+  this.txt = "";
+  this.tick();
+  this.isDeleting = false;
 };
-
-TxtType.prototype.tick = function() {
-    var i = this.loopNum % this.toRotate.length;
-    var fullTxt = this.toRotate[i];
-
-    if (this.isDeleting) {
+TxtType.prototype.tick = function () {
+  var i = this.loopNum % this.toRotate.length;
+  var fullTxt = this.toRotate[i];
+  if (this.isDeleting) {
     this.txt = fullTxt.substring(0, this.txt.length - 1);
-    } else {
+  } else {
     this.txt = fullTxt.substring(0, this.txt.length + 1);
-    }
-
-    this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
-
-    var that = this;
-    var delta = 200 - Math.random() * 100;
-
-    if (this.isDeleting) { delta /= 2; }
-
-    if (!this.isDeleting && this.txt === fullTxt) {
+  }
+  this.el.innerHTML = '<span class="wrap">' + this.txt + "</span>";
+  var that = this;
+  var delta = 200 - Math.random() * 100;
+  if (this.isDeleting) {
+    delta /= 2;
+  }
+  if (!this.isDeleting && this.txt === fullTxt) {
     delta = this.period;
     this.isDeleting = true;
-    } else if (this.isDeleting && this.txt === '') {
+  } else if (this.isDeleting && this.txt === "") {
     this.isDeleting = false;
     this.loopNum++;
     delta = 500;
-    }
-
-    setTimeout(function() {
+  }
+  setTimeout(function () {
     that.tick();
-    }, delta);
+  }, delta);
 };
-
-window.onload = function() {
-    var elements = document.getElementsByClassName('typewrite');
-    for (var i=0; i<elements.length; i++) {
-        var toRotate = elements[i].getAttribute('data-type');
-        var period = elements[i].getAttribute('data-period');
-        if (toRotate) {
-          new TxtType(elements[i], JSON.parse(toRotate), period);
-        }
+document.addEventListener("DOMContentLoaded", function () {
+  /* Init typewriter */
+  var elements = document.getElementsByClassName("typewrite");
+  for (var i = 0; i < elements.length; i++) {
+    var toRotate = elements[i].getAttribute("data-type");
+    var period = elements[i].getAttribute("data-period");
+    if (toRotate) {
+      new TxtType(elements[i], JSON.parse(toRotate), period);
     }
-    // INJECT CSS
-    var css = document.createElement("style");
-    // css.type = "text/css";
-    css.innerHTML = ".typewrite > .wrap { border-right: 0em solid #fff}";
-    document.body.appendChild(css);
-};
+  }
+  /* Mobile nav toggle */
+  var navToggle = document.getElementById("nav-toggle");
+  var navMenu = document.getElementById("nav-menu");
+  navToggle.addEventListener("click", function () {
+    navMenu.classList.toggle("open");
+    navToggle.classList.toggle("active");
+    var expanded = navToggle.getAttribute("aria-expanded") === "true";
+    navToggle.setAttribute("aria-expanded", !expanded);
+  });
+  /* Close mobile nav on link click */
+  document.querySelectorAll(".nav-link").forEach(function (link) {
+    link.addEventListener("click", function () {
+      navMenu.classList.remove("open");
+      navToggle.classList.remove("active");
+      navToggle.setAttribute("aria-expanded", "false");
+    });
+  });
+  /* Navbar scroll effect */
+  var navbar = document.getElementById("navbar");
+  window.addEventListener("scroll", function () {
+    if (window.scrollY > 50) {
+      navbar.classList.add("scrolled");
+    } else {
+      navbar.classList.remove("scrolled");
+    }
+  });
+  /* Active nav link on scroll */
+  var sections = document.querySelectorAll("section[id]");
+  var navLinks = document.querySelectorAll(".nav-link");
+  window.addEventListener("scroll", function () {
+    var scrollPos = window.scrollY + 100;
+    sections.forEach(function (section) {
+      var top = section.offsetTop;
+      var height = section.offsetHeight;
+      var id = section.getAttribute("id");
+      if (scrollPos >= top && scrollPos < top + height) {
+        navLinks.forEach(function (link) {
+          link.classList.remove("active");
+          if (link.getAttribute("href") === "#" + id) {
+            link.classList.add("active");
+          }
+        });
+      }
+    });
+  });
+  /* Scroll reveal animation */
+  var revealElements = document.querySelectorAll(
+    ".timeline-item, .skill-category, .project-card, .about-grid, .highlight-card, .contact-card"
+  );
+  revealElements.forEach(function (el) {
+    el.classList.add("reveal");
+  });
+  var observer = new IntersectionObserver(
+    function (entries) {
+      entries.forEach(function (entry) {
+        if (entry.isIntersecting) {
+          entry.target.classList.add("visible");
+        }
+      });
+    },
+    { threshold: 0.1, rootMargin: "0px 0px -50px 0px" }
+  );
+  revealElements.forEach(function (el) {
+    observer.observe(el);
+  });
+});
